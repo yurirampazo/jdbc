363479b301d71c4760f50b487c095a1b108f3840
Yuri <yuri.rampazo@outlook.com>
2021-08-12 14:42:00 -0300

    JDBC Introduction

---
+++ jdbc6/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.*;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement st = null;
+        try {
+            conn = DB.getConnection();
+            conn.setAutoCommit(false); // Não é pra confirmar as operações automaticamente
+
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090.0 " +
+                    "WHERE DepartmentiD = 1");
+
+            //int x = 1;
+
+           //if (x < 2) throw new SQLException("Fake Error");
+
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090.0 " +
+                    "WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("Rows 1 = " + rows1);
+            System.out.println("Rows 2 = " + rows2);
+        }
+        catch (SQLException e) {
+            try {
+                conn.rollback();
+                throw new DbException("Transaction Rolled Back! Caused By: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw  new DbException("Error Trying to Rollback! Caused by: " + ex.getMessage());
+            }
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
